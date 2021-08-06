f386469e333bdefea64cdd53eb740de57d423cc8
Mohamed18995 <ay1665ay@gmail.com>
2021-08-06 19:00:55 +0200

    update

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 const menuIcon = document.querySelector(".menu-icon");
 const mobileMenu = document.querySelector(".nav-menu");
+const icon = document.querySelector(".fa-bars");
+const closeIcon =document.querySelector(".fa-times");
  
 menuIcon.addEventListener("click", () => {
     mobileMenu.classList.toggle("mobile-menu");
+    icon.classList.toggle("hide-menu");
+    closeIcon.classList.toggle("show-close");
+
 });
